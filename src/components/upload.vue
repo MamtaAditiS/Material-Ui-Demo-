aed5923fc67142e7a3b0ec90f542de5e5d4527d6
Mamta Singla <mamtasingla007@gmail.com>
2017-08-04 18:51:37 +0530

    solved issues regarding name conflicts of filename hello to upload

--- src/components/upload.vue
+++ src/components/upload.vue
@@ -8,7 +8,7 @@
 
 <script>
 export default {
-  name: 'hello',
+  name: 'upload',
   data () {
     return {
       msg: 'Welcome to Vue.js File Uploader'
@@ -17,7 +17,7 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
 h1, h2 {
   font-weight: normal;
